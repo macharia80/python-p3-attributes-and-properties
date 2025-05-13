547d49f777f54cca94b622636930ac633d5d58c4
macharia <mrmachariaorsam@gmail.com>
2025-05-13 21:42:32 +0300

    last commit

--- lib/dog.py
+++ lib/dog.py
@@ -12,4 +12,37 @@ APPROVED_BREEDS = [
 ]
 
 class Dog:
+    # lib/dog.py
+
+
+    approved_breeds = ["Mastiff", "Chihuahua", "Corgi", "Shar Pei", "Beagle",
+                       "French Bulldog", "Pug", "Pointer"]
+
+    def __init__(self, name="fido", breed="Mastiff"):
+        self._name = None  # Initialize protected attribute
+        self._breed = None
+        self.name = name   # This triggers the setter
+        self.breed = breed # This triggers the setter
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        if isinstance(value, str) and 1 <= len(value) <= 25:
+            self._name = value
+        else:
+            print("Name must be string between 1 and 25 characters.")
+
+    @property
+    def breed(self):
+        return self._breed
+
+    @breed.setter
+    def breed(self, value):
+        if value in Dog.approved_breeds:
+            self._breed = value
+        else:
+            print("Breed must be in list of approved breeds.")
     pass
--- lib/person.py
+++ lib/person.py
@@ -16,4 +16,40 @@ APPROVED_JOBS = [
 ]
 
 class Person:
+    # lib/person.py
+
+
+    approved_jobs = [
+        "Admin", "Customer Service", "Human Resources", "ITC", "Production",
+        "Legal", "Finance", "Sales", "General Management", "Research & Development",
+        "Marketing", "Purchasing"
+    ]
+
+    def __init__(self, name="Guido", job="Sales"):
+        self._name = None  # Initialize protected attribute
+        self._job = None
+        self.name = name   # This triggers the setter
+        self.job = job     # This triggers the setter
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        if isinstance(value, str) and 1 <= len(value) <= 25:
+            self._name = value.title()
+        else:
+            print("Name must be string between 1 and 25 characters.")
+
+    @property
+    def job(self):
+        return self._job
+
+    @job.setter
+    def job(self, value):
+        if value in Person.approved_jobs:
+            self._job = value
+        else:
+            print("Job must be in list of approved jobs.")
     pass
